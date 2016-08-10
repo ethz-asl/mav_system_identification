c154a276de76062a7d3fe176bd5199b8244fb000
Inkyu <Inkyu@guest-docking-lee-1-0197.ethz.ch>
2016-08-10 20:17:42 +0200

    Adding comments on matlab_rosbag package. Change the name of path (typo).

--- Example/script.m
+++ Example/script.m
@@ -1,4 +1,19 @@
-path(path, '../rad_bagd');
+% 
+% In order to run this script you need matlab_rosbag package
+% https://github.com/bcharrow/matlab_rosbag (source)
+% https://github.com/bcharrow/matlab_rosbag/releases (binary)
+% 
+% In case you face the follosing linking error
+% matlab_rosbag-0.5.0-mac64/rosbag_wrapper.mexmaci64,
+% 6): Symbol not found: __ZTISt16invalid_argument
+% try this re-compiled binary
+% https://cmu.app.box.com/s/9hs153nwa19uqvzboglkz7y84r6jzzxg    or
+% https://dl.dropboxusercontent.com/u/12446150/matlab_rosbag-0.5.0-mac64_matlabR2015a.zip
+% Tested platform: Mac EI Capitan 10.11.6 with MATLAB R2016a
+%
+
+path(path, '../read_bags');
+
 path(path, '../helper_functions');
 
 % two experiments are needed to validate the identification
